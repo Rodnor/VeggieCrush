75095fc88c1432aab7b2098ac8959ae9c85eacd2
Rodnor <tristan.lascombe@gmail.com>
2017-11-13 20:07:27 +0100

    Rajout des conditions de fin de partie + comptage du nombre de coups restants + timer + ajout des images d'herbe sur le côté + empêcher de cliquer 2 fois d'affilée sur le même bouton + ajustements graphiques du canevas de jeu

--- VeggieCrush/src/PanelJeu.java
+++ VeggieCrush/src/PanelJeu.java
@@ -1,5 +1,4 @@
 import java.awt.Color;
-import java.awt.Font;
 import java.awt.Graphics;
 import javax.swing.JPanel;
 import net.miginfocom.swing.MigLayout;
@@ -8,7 +7,6 @@ import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
-import javax.swing.JTable;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -23,60 +21,98 @@ public class PanelJeu extends JPanel implements ActionListener {
 	private ImageIcon prev;
 	private ImageIcon next;
 	private JButton prevJButton;
-  	private BufferedImage iconPlante=null;
+	private BufferedImage iconPlante=null;
+	private int nombreCoups=10;
+	private int nombreCoupsBonus=0;
+	private int tempsBonus=0;
+	private int tempsBase=30;
+	private int tempsTotal=tempsBase+tempsBonus;
+	private int scoreBonus=0;
+	private int herbe1Bonus=0;
+	private int herbe2Bonus=0;
+	private int herbe3Bonus=0;
+	private int herbe4Bonus=0;
+	private JLabel nbCoupsRestants;
+	private JLabel lblTimer;
+	private Thread t;
+	private boolean stopGame=false;
 
 	public PanelJeu(){
-		setLayout(new MigLayout("", "[][grow][][][]", "[][grow][grow][grow][grow][grow]"));
+		setLayout(new MigLayout("", "[][grow][][][][grow]", "[][][grow][grow][grow][grow][grow]"));
 
-		JLabel lblTimer = new JLabel("Timer");
+		lblTimer = new JLabel("Temps restant : "+String.valueOf(tempsTotal));
 		add(lblTimer, "cell 0 0");
 
-		JLabel lblScore = new JLabel("Score");
-		add(lblScore, "cell 4 0");
+		JLabel lblScore = new JLabel(String.valueOf("Score : "+(0+scoreBonus)));
+		add(lblScore, "cell 5 0");
+
+		nbCoupsRestants = new JLabel(String.valueOf("Nombre de coups restants : "+(nombreCoups+nombreCoupsBonus)));
+		add(nbCoupsRestants, "cell 0 1");
 
 		JPanel panel_4 = new JPanel();
-		add(panel_4, "cell 1 1 1 5,grow");
-		panel_4.setLayout(new GridLayout(9, 9, 0, 0));
+		add(panel_4, "cell 0 2 2 5,alignx center,aligny center");
+		panel_4.setLayout(new GridLayout(9, 9, -14, -10));
 
-		JPanel panel = new JPanel();
-		panel.setBackground(Color.RED);
-		add(panel, "cell 2 2,alignx center,aligny center");
+		ImageIcon icon=null;
+		try {
+			icon = new ImageIcon(ImageIO.read(new File("images/herbe1.jpg")));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} 
+		JLabel thumb = new JLabel();
+		thumb.setIcon(icon);
+		add(thumb, "cell 2 3,alignx center,aligny center");
 
 		JLabel lblX = new JLabel("x");
-		add(lblX, "cell 3 2");
+		add(lblX, "cell 3 3");
 
-		JLabel label = new JLabel("0");
-		add(label, "cell 4 2");
+		JLabel label = new JLabel(String.valueOf(0+herbe1Bonus));
+		add(label, "cell 4 3");
 
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(Color.GREEN);
-		add(panel_1, "cell 2 3,alignx center,aligny center");
+		try {
+			icon = new ImageIcon(ImageIO.read(new File("images/herbe2.jpg")));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} 
+		thumb = new JLabel();
+		thumb.setIcon(icon);
+		add(thumb, "cell 2 4,alignx center,aligny center");
 
 		JLabel lblNewLabel = new JLabel("x");
-		add(lblNewLabel, "cell 3 3");
+		add(lblNewLabel, "cell 3 4");
 
-		JLabel label_1 = new JLabel("0");
-		add(label_1, "cell 4 3");
+		JLabel label_1 = new JLabel(String.valueOf(0+herbe2Bonus));
+		add(label_1, "cell 4 4");
 
-		JPanel panel_2 = new JPanel();
-		panel_2.setBackground(Color.BLUE);
-		add(panel_2, "cell 2 4,alignx center,aligny center");
+		try {
+			icon = new ImageIcon(ImageIO.read(new File("images/herbe3.jpg")));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} 
+		thumb = new JLabel();
+		thumb.setIcon(icon);
+		add(thumb, "cell 2 5,alignx center,aligny center");
 
 		JLabel lblNewLabel_1 = new JLabel("x");
-		add(lblNewLabel_1, "cell 3 4");
+		add(lblNewLabel_1, "cell 3 5");
 
-		JLabel label_2 = new JLabel("0");
-		add(label_2, "cell 4 4");
+		JLabel label_2 = new JLabel(String.valueOf(0+herbe3Bonus));
+		add(label_2, "cell 4 5");
 
-		JPanel panel_3 = new JPanel();
-		panel_3.setBackground(Color.DARK_GRAY);
-		add(panel_3, "cell 2 5,alignx center,aligny center");
+		try {
+			icon = new ImageIcon(ImageIO.read(new File("images/herbe4.jpg")));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} 
+		thumb = new JLabel();
+		thumb.setIcon(icon);
+		add(thumb, "cell 2 6,alignx center,aligny center");
 
 		JLabel lblNewLabel_2 = new JLabel("x");
-		add(lblNewLabel_2, "cell 3 5");
+		add(lblNewLabel_2, "cell 3 6");
 
-		JLabel label_3 = new JLabel("0");
-		add(label_3, "cell 4 5");
+		JLabel label_3 = new JLabel(String.valueOf(0+herbe4Bonus));
+		add(label_3, "cell 4 6");
 
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++)
@@ -84,10 +120,8 @@ public class PanelJeu extends JPanel implements ActionListener {
 				try {
 					iconPlante = ImageIO.read(new File("images/herbe"+String.valueOf((int)((Math.random()*4)+1))+".jpg"));
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				//b[i][j] = new JButton("("+(i+1)+") "+"("+(j+1)+")");
 				b[i][j] = new JButton(new ImageIcon(iconPlante));
 				b[i][j].setBorderPainted(false);
 				b[i][j].setFocusPainted(false);
@@ -97,6 +131,42 @@ public class PanelJeu extends JPanel implements ActionListener {
 				panel_4.add(b[i][j]);
 			}
 		}
+
+		t = new Thread() {
+			public void run() {
+				while(tempsTotal>=0 && stopGame==false) {
+					lblTimer.setText("Temps restant : "+String.valueOf(tempsTotal));
+					try {
+						Thread.sleep(1000);
+						tempsTotal--;
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					// Fin de la partie
+					if(tempsTotal == 0) {
+						System.out.println("Fin du game !");
+					}
+				}
+			}
+		};
+		t.start();
+		
+		Thread t2 = new Thread() {
+			public void run() {
+				while(!stopGame) {
+					if(nombreCoups==0){
+						stopGame=true;
+						System.out.println("Fin du game !");
+					}
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		};
+		t2.start();
 	}
 
 	public void paintComponent(Graphics g){
@@ -106,28 +176,34 @@ public class PanelJeu extends JPanel implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 
-		if(numClic == 0) {
-			for (int row = 0; row < 9; row++) {
-				for (int col = 0; col < 9; col++) {
-					if (b[row][col] == e.getSource()){
-						prev = (ImageIcon) b[row][col].getIcon();
-						prevJButton = b[row][col];
-						numClic++;
+		if(nombreCoups>0) {
+			if(numClic == 0) {
+				for (int row = 0; row < 9; row++) {
+					for (int col = 0; col < 9; col++) {
+						if (b[row][col] == e.getSource()){
+							prev = (ImageIcon) b[row][col].getIcon();
+							prevJButton = b[row][col];
+							numClic++;
+						}
 					}
 				}
-			}
-		} else {
-			for (int row = 0; row < 9; row++) {
-				for (int col = 0; col < 9; col++) {
-					if (b[row][col] == e.getSource()){
-						next = (ImageIcon) b[row][col].getIcon();
-						b[row][col].setIcon(prev);
-						prevJButton.setIcon(next);
-						numClic++;
+			} else {
+				for (int row = 0; row < 9; row++) {
+					for (int col = 0; col < 9; col++) {
+						if (b[row][col] == e.getSource()){
+							if(b[row][col] != prevJButton) {
+								next = (ImageIcon) b[row][col].getIcon();
+								b[row][col].setIcon(prev);
+								prevJButton.setIcon(next);
+								numClic++;
+								nombreCoups--;
+							}
+						}
 					}
 				}
+				numClic = 0;
 			}
-			numClic = 0;
+			nbCoupsRestants.setText("Nombre de coups restants : "+String.valueOf(nombreCoups));
 		}
 	}
 }
