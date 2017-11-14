44b0fd765c2dbec1b27dfe5ccc83066a7566d032
Rodnor <tristan.lascombe@gmail.com>
2017-11-14 10:34:50 +0100

    rajout d'un bouton start pour commencer le jeu + switch uniquement les cases qui se touchent (mais pas les diagonales)

--- VeggieCrush/src/PanelJeu.java
+++ VeggieCrush/src/PanelJeu.java
@@ -22,10 +22,10 @@ public class PanelJeu extends JPanel implements ActionListener {
 	private ImageIcon next;
 	private JButton prevJButton;
 	private BufferedImage iconPlante=null;
-	private int nombreCoups=10;
+	private int nombreCoups=1000;
 	private int nombreCoupsBonus=0;
 	private int tempsBonus=0;
-	private int tempsBase=30;
+	private int tempsBase=9999;
 	private int tempsTotal=tempsBase+tempsBonus;
 	private int scoreBonus=0;
 	private int herbe1Bonus=0;
@@ -34,23 +34,27 @@ public class PanelJeu extends JPanel implements ActionListener {
 	private int herbe4Bonus=0;
 	private JLabel nbCoupsRestants;
 	private JLabel lblTimer;
-	private Thread t;
 	private boolean stopGame=false;
+	private boolean gameRunning=false;
+	private boolean canSwitch=false;
+	private int row;
+	private int col;
 
 	public PanelJeu(){
-		setLayout(new MigLayout("", "[][grow][][][][grow]", "[][][grow][grow][grow][grow][grow]"));
+		
+		setLayout(new MigLayout("", "[][][grow][][][][grow]", "[][][grow][grow][grow][grow][grow][]"));
 
 		lblTimer = new JLabel("Temps restant : "+String.valueOf(tempsTotal));
 		add(lblTimer, "cell 0 0");
 
 		JLabel lblScore = new JLabel(String.valueOf("Score : "+(0+scoreBonus)));
-		add(lblScore, "cell 5 0");
+		add(lblScore, "cell 6 0");
 
 		nbCoupsRestants = new JLabel(String.valueOf("Nombre de coups restants : "+(nombreCoups+nombreCoupsBonus)));
 		add(nbCoupsRestants, "cell 0 1");
 
 		JPanel panel_4 = new JPanel();
-		add(panel_4, "cell 0 2 2 5,alignx center,aligny center");
+		add(panel_4, "cell 0 2 3 5,alignx center,aligny center");
 		panel_4.setLayout(new GridLayout(9, 9, -14, -10));
 
 		ImageIcon icon=null;
@@ -61,13 +65,13 @@ public class PanelJeu extends JPanel implements ActionListener {
 		} 
 		JLabel thumb = new JLabel();
 		thumb.setIcon(icon);
-		add(thumb, "cell 2 3,alignx center,aligny center");
+		add(thumb, "cell 3 3,alignx center,aligny center");
 
 		JLabel lblX = new JLabel("x");
-		add(lblX, "cell 3 3");
+		add(lblX, "cell 4 3");
 
 		JLabel label = new JLabel(String.valueOf(0+herbe1Bonus));
-		add(label, "cell 4 3");
+		add(label, "cell 5 3");
 
 		try {
 			icon = new ImageIcon(ImageIO.read(new File("images/herbe2.jpg")));
@@ -76,13 +80,13 @@ public class PanelJeu extends JPanel implements ActionListener {
 		} 
 		thumb = new JLabel();
 		thumb.setIcon(icon);
-		add(thumb, "cell 2 4,alignx center,aligny center");
+		add(thumb, "cell 3 4,alignx center,aligny center");
 
 		JLabel lblNewLabel = new JLabel("x");
-		add(lblNewLabel, "cell 3 4");
+		add(lblNewLabel, "cell 4 4");
 
 		JLabel label_1 = new JLabel(String.valueOf(0+herbe2Bonus));
-		add(label_1, "cell 4 4");
+		add(label_1, "cell 5 4");
 
 		try {
 			icon = new ImageIcon(ImageIO.read(new File("images/herbe3.jpg")));
@@ -91,13 +95,13 @@ public class PanelJeu extends JPanel implements ActionListener {
 		} 
 		thumb = new JLabel();
 		thumb.setIcon(icon);
-		add(thumb, "cell 2 5,alignx center,aligny center");
+		add(thumb, "cell 3 5,alignx center,aligny center");
 
 		JLabel lblNewLabel_1 = new JLabel("x");
-		add(lblNewLabel_1, "cell 3 5");
+		add(lblNewLabel_1, "cell 4 5");
 
 		JLabel label_2 = new JLabel(String.valueOf(0+herbe3Bonus));
-		add(label_2, "cell 4 5");
+		add(label_2, "cell 5 5");
 
 		try {
 			icon = new ImageIcon(ImageIO.read(new File("images/herbe4.jpg")));
@@ -106,13 +110,18 @@ public class PanelJeu extends JPanel implements ActionListener {
 		} 
 		thumb = new JLabel();
 		thumb.setIcon(icon);
-		add(thumb, "cell 2 6,alignx center,aligny center");
+		add(thumb, "cell 3 6,alignx center,aligny center");
 
 		JLabel lblNewLabel_2 = new JLabel("x");
-		add(lblNewLabel_2, "cell 3 6");
+		add(lblNewLabel_2, "cell 4 6");
 
 		JLabel label_3 = new JLabel(String.valueOf(0+herbe4Bonus));
-		add(label_3, "cell 4 6");
+		add(label_3, "cell 5 6");
+		
+		JButton btnJouer = new JButton("Jouer !");
+		btnJouer.addActionListener(this);
+		btnJouer.setActionCommand("jouer");
+		add(btnJouer, "cell 1 7");
 
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++)
@@ -131,8 +140,69 @@ public class PanelJeu extends JPanel implements ActionListener {
 				panel_4.add(b[i][j]);
 			}
 		}
+	}
 
-		t = new Thread() {
+	public void paintComponent(Graphics g){
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() instanceof JButton) {
+			JButton btn = (JButton) e.getSource();
+			
+			if(btn.getActionCommand().equals("jouer")) {
+				startThreads();
+			} else {
+				if(nombreCoups>0 && gameRunning) {
+					if(numClic == 0) {
+						for (int row = 0; row < 9; row++) {
+							for (int col = 0; col < 9; col++) {
+								if (b[row][col] == e.getSource()){
+									prev = (ImageIcon) b[row][col].getIcon();
+									prevJButton = b[row][col];
+									this.col=col;
+									this.row=row;
+									numClic++;
+								}
+							}
+						}
+					} else {
+						for (int row = 0; row < 9; row++) {
+							for (int col = 0; col < 9; col++) {
+								if (b[row][col] == e.getSource()){
+									/*System.out.println("Ligne case précédente : "+this.row);
+									System.out.println("colonne case précédente : "+this.col);
+									System.out.println("Ligne courante : "+row);
+									System.out.println("Colonne courante : "+col);
+									System.out.println("Ligne suivante : "+(row+1));
+									System.out.println("Colonne suivante : "+(col+1));
+									System.out.println("Ligne précédente : "+(row-1));
+									System.out.println("Colonne précédente : "+(col-1));*/
+									if(row+1 == this.row && col == this.col || row-1 == this.row && col == this.col || row == this.row && col+1 == this.col || row == this.row && col-1 == this.col)
+											canSwitch=true;
+									
+									if(b[row][col] != prevJButton && canSwitch) {
+										next = (ImageIcon) b[row][col].getIcon();
+										b[row][col].setIcon(prev);
+										prevJButton.setIcon(next);
+										nombreCoups--;
+										canSwitch=false;
+									}
+								}
+							}
+						}
+						numClic = 0;
+					}
+					nbCoupsRestants.setText("Nombre de coups restants : "+String.valueOf(nombreCoups));
+				}
+			}
+		}
+	}
+	
+	public void startThreads() {
+		gameRunning = true;
+		Thread t = new Thread() {
 			public void run() {
 				while(tempsTotal>=0 && stopGame==false) {
 					lblTimer.setText("Temps restant : "+String.valueOf(tempsTotal));
@@ -144,6 +214,7 @@ public class PanelJeu extends JPanel implements ActionListener {
 					}
 					// Fin de la partie
 					if(tempsTotal == 0) {
+						gameRunning = false;
 						System.out.println("Fin du game !");
 					}
 				}
@@ -156,6 +227,7 @@ public class PanelJeu extends JPanel implements ActionListener {
 				while(!stopGame) {
 					if(nombreCoups==0){
 						stopGame=true;
+						gameRunning = false;
 						System.out.println("Fin du game !");
 					}
 					try {
@@ -167,43 +239,5 @@ public class PanelJeu extends JPanel implements ActionListener {
 			}
 		};
 		t2.start();
-	}
-
-	public void paintComponent(Graphics g){
-
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-
-		if(nombreCoups>0) {
-			if(numClic == 0) {
-				for (int row = 0; row < 9; row++) {
-					for (int col = 0; col < 9; col++) {
-						if (b[row][col] == e.getSource()){
-							prev = (ImageIcon) b[row][col].getIcon();
-							prevJButton = b[row][col];
-							numClic++;
-						}
-					}
-				}
-			} else {
-				for (int row = 0; row < 9; row++) {
-					for (int col = 0; col < 9; col++) {
-						if (b[row][col] == e.getSource()){
-							if(b[row][col] != prevJButton) {
-								next = (ImageIcon) b[row][col].getIcon();
-								b[row][col].setIcon(prev);
-								prevJButton.setIcon(next);
-								numClic++;
-								nombreCoups--;
-							}
-						}
-					}
-				}
-				numClic = 0;
-			}
-			nbCoupsRestants.setText("Nombre de coups restants : "+String.valueOf(nombreCoups));
-		}
 	}
 }
