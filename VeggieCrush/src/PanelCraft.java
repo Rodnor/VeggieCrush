68536f919aa973419d459c646948b3cd33a268f2
keket87 <keket87@homail.fr>
2017-11-15 20:01:52 +0100

    Fenetre craft

--- VeggieCrush/src/PanelCraft.java
+++ VeggieCrush/src/PanelCraft.java
@@ -6,9 +6,12 @@ import javax.swing.JLabel;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+
+import java.awt.AWTException;
 import java.awt.BorderLayout;
 import javax.swing.JTable;
 import java.awt.GridLayout;
+import java.awt.Robot;
 import java.awt.Dimension;
 import net.miginfocom.swing.MigLayout;
 import javax.swing.*;
@@ -18,219 +21,192 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import java.awt.FlowLayout;
 
 public class PanelCraft extends JPanel{
 	  private Color color = Color.white;
 	  private String message = "";
 	   
-	  public PanelCraft(){
-	  	setLayout(new MigLayout("", "[][grow][]", "[][grow][][][75px:75px:75px,grow][][][grow][][]"));
-	  	
-	  	JPanel panel_1 = new JPanel();
-	  	add(panel_1, "cell 1 1,grow");
-	  	
-	  	JPanel panel = new JPanel();
-	  	add(panel, "cell 1 4,grow");
-	  	panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-	  	
-	  	/*JButton btn_inv_plante1 = new JButton("New button");
-	  	JButton btn_inv_plante2 = new JButton("New button");
-	  	JButton btn_inv_plante3 = new JButton("New button");
-	  	JButton btn_inv_plante4 = new JButton("New button");*/
+	  /**
+	 * 
+	 */
+	  
+	  protected JComponent makeTextPanel(String text) {
+	        JPanel panel = new JPanel(false);
+	        JLabel filler = new JLabel(text);
+	        filler.setHorizontalAlignment(JLabel.CENTER);
+	        panel.setLayout(new GridLayout(1, 1));
+	        panel.add(filler);
+	        return panel;
+	    }
+	  
+	public PanelCraft(){
+	  	setLayout(new MigLayout("", "[][grow][]", "[][100px:100px:100px,grow][grow][][120px:120px:120px,grow][80px:80px:80px,grow][][]"));
 	  	
 	  	BufferedImage iconPlante1=null;
 	  	BufferedImage iconPlante2=null;
 	  	BufferedImage iconPlante3=null;
 	  	BufferedImage iconPlante4=null;
+	  	BufferedImage iconInventaire=null;
+	  	BufferedImage iconCraft=null;
+	  	
 		try {
 			iconPlante1 = ImageIO.read(new File("images/herbe1.jpg"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 	  	try {
 			iconPlante2 = ImageIO.read(new File("images/herbe2.jpg"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	  	
 	  	try {
 			iconPlante3 = ImageIO.read(new File("images/herbe3.jpg"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	  	
 	  	try {
 			iconPlante4 = ImageIO.read(new File("images/herbe4.jpg"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	  	/*btn_inv_plante1 = new JButton(new ImageIcon(iconPlante1));
-	  	btn_inv_plante1.setBorderPainted(false);
-	  	btn_inv_plante1.setFocusPainted(false);
-	  	btn_inv_plante1.setContentAreaFilled(false);
-	  	btn_inv_plante2 = new JButton(new ImageIcon(iconPlante2));
-	  	btn_inv_plante2.setBorderPainted(false);
-	  	btn_inv_plante2.setFocusPainted(false);
-	  	btn_inv_plante2.setContentAreaFilled(false);
-	  	btn_inv_plante3 = new JButton(new ImageIcon(iconPlante3));
-	  	btn_inv_plante3.setBorderPainted(false);
-	  	btn_inv_plante3.setFocusPainted(false);
-	  	btn_inv_plante3.setContentAreaFilled(false);
-	  	btn_inv_plante4 = new JButton(new ImageIcon(iconPlante4));
-	  	btn_inv_plante4.setBorderPainted(false);
-	  	btn_inv_plante4.setFocusPainted(false);
-	  	btn_inv_plante4.setContentAreaFilled(false);
+	  		  	
+	  	try {
+	  		iconInventaire = ImageIO.read(new File("images/crate_inventaire1.png"));
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	  	
-	  	btn_inv_plante1.setSize(75, 75);
-	  	btn_inv_plante2.setSize(75, 75);
-	  	btn_inv_plante3.setSize(75, 75);
-	  	btn_inv_plante4.setSize(75, 75);
+	  	try {
+	  		iconCraft = ImageIO.read(new File("images/craft.png"));
+	  		
+	  	} catch (IOException e) {
+	  		// TODO Auto-generated catch block
+	  		e.printStackTrace();
+	  	}
 	  	
-	  	btn_inv_plante1.addActionListener(new ActionListener() {
-	  	    public void actionPerformed(ActionEvent e) {
-	  	        System.out.println("Plante 1");
-	  	    }
-	  	});
-	  	btn_inv_plante2.addActionListener(new ActionListener() {
-	  	    public void actionPerformed(ActionEvent e) {
-	  	        System.out.println("Plante 2");
-	  	    }
-	  	});
-	  	btn_inv_plante3.addActionListener(new ActionListener() {
-	  	    public void actionPerformed(ActionEvent e) {
-	  	        System.out.println("Plante 3");
-	  	    }
-	  	});
-	  	btn_inv_plante4.addActionListener(new ActionListener() {
-	  	    public void actionPerformed(ActionEvent e) {
-	  	        System.out.println("Plante 4");
-	  	    }
-	  	});
+	  	JPanel panel_1 = new JPanel();
+	  	add(panel_1, "cell 1 1,grow");
 	  	
-	  	panel.add(btn_inv_plante1);
-	  	panel.add(btn_inv_plante2);
-	  	panel.add(btn_inv_plante3);
-	  	panel.add(btn_inv_plante4);*/
+	  	JTabbedPane tabbedPane = new JTabbedPane();
+	  	
+	  	JComponent panel1 = makeTextPanel("Panel #1");
+
+	  	tabbedPane.addTab("Tab 1", panel1);
+
+	  	JComponent panel2 = makeTextPanel("Panel #2");
+	  	tabbedPane.addTab("Tab 2", panel2);
+
+	  	JComponent panel3 = makeTextPanel("Panel #3");
+	  	tabbedPane.addTab("Tab 3", panel3);
+
+	  	JComponent panel4 = makeTextPanel(
+	  	        "Panel #4");
+	  	
+	  	panel1.setPreferredSize(new Dimension(500, 60));
+	  	panel2.setPreferredSize(new Dimension(500, 60));
+	  	panel3.setPreferredSize(new Dimension(500, 60));
+	  	panel4.setPreferredSize(new Dimension(500, 60));
+	  	tabbedPane.addTab("Tab 4", panel4);
+	  	
+	  	panel_1.add(tabbedPane);
+	  	
+	  	JPanel panel_4 = new JPanel();
+	  	add(panel_4, "cell 1 2,grow");
+	  	
+	  	JLabel lblNewLabel = new JLabel("Informations sur recette :");
+	  	panel_4.add(lblNewLabel);
+	  	
+	  	
+	  	JPanel panel_2 = new JPanel();
+	  	add(panel_2, "cell 1 4,grow");
+	  	
+	  	JButton btnInventaire= new JButton(new ImageIcon(iconInventaire));
+	  	btnInventaire.setBorderPainted(false);
+	  	btnInventaire.setFocusPainted(false);
+	  	btnInventaire.setContentAreaFilled(false);
+	  	panel_2.add(btnInventaire);
+	  	
+	  	JPanel panel = new JPanel();
+	  	add(panel, "cell 1 5,grow");
+	  	panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+
+	  	
 	  	
 	  	JLabel inv_plante1,inv_plante2,inv_plante3, inv_plante4;
 	  	inv_plante1 = new JLabel();
 	  	inv_plante2 = new JLabel();
 	  	inv_plante3 = new JLabel();
 	  	inv_plante4 = new JLabel();
+	  	inv_plante1.setName("1");
+	  	inv_plante2.setName("2");
+	  	inv_plante3.setName("3");
+	  	inv_plante4.setName("4");
+	  	inv_plante1.setText("1 Dispo");
+	  	inv_plante1.setVerticalTextPosition(JLabel.BOTTOM);
+	  	inv_plante1.setHorizontalTextPosition(JLabel.CENTER);
+	  	inv_plante2.setText("11 Dispo");
+	  	inv_plante2.setVerticalTextPosition(JLabel.BOTTOM);
+	  	inv_plante2.setHorizontalTextPosition(JLabel.CENTER);
+	  	inv_plante3.setText("18 Dispo");
+	  	inv_plante3.setVerticalTextPosition(JLabel.BOTTOM);
+	  	inv_plante3.setHorizontalTextPosition(JLabel.CENTER);
+	  	inv_plante4.setText("9 Dispo");
+	  	inv_plante4.setVerticalTextPosition(JLabel.BOTTOM);
+	  	inv_plante4.setHorizontalTextPosition(JLabel.CENTER);
 	  	inv_plante1.setIcon(new ImageIcon(iconPlante1));
 	  	inv_plante2.setIcon(new ImageIcon(iconPlante2));
 	  	inv_plante3.setIcon(new ImageIcon(iconPlante3));
 	  	inv_plante4.setIcon(new ImageIcon(iconPlante4));
-	  	
-	  	MouseListener ml = new MouseListener() {
-
-            @Override
-            public void mouseClicked(MouseEvent e) {}
-
-            @Override
-            public void mousePressed(MouseEvent e) {
-            
-                JComponent jc = (JComponent)e.getSource();
-                TransferHandler th = jc.getTransferHandler();
-                th.exportAsDrag(jc, e, TransferHandler.COPY);
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {}
-
-            @Override
-            public void mouseEntered(MouseEvent e) {}
-
-            @Override
-            public void mouseExited(MouseEvent e) {}
-        };
-        
-        inv_plante1.addMouseListener(ml);
-        inv_plante2.addMouseListener(ml);
-        inv_plante3.addMouseListener(ml);
-        inv_plante4.addMouseListener(ml);
-        
-        inv_plante1.setTransferHandler(new TransferHandler("icon"));
-        inv_plante2.setTransferHandler(new TransferHandler("icon"));
-        inv_plante3.setTransferHandler(new TransferHandler("icon"));
-        inv_plante4.setTransferHandler(new TransferHandler("icon"));
         
         panel.add(inv_plante1);
+        
+        JSpinner spinner = new JSpinner(new SpinnerNumberModel(0.0, 0.0, 999.0,
+                1));
+        
+        int w = spinner.getWidth();   int h = spinner.getHeight();
+        panel.add(spinner);
         panel.add(inv_plante2);
+        
+        JSpinner spinner_1 = new JSpinner(new SpinnerNumberModel(0.0, 0.0, 999.0,
+                1));
+        spinner_1.setMinimumSize(new Dimension(w*2,h));
+        panel.add(spinner_1);
         panel.add(inv_plante3);
+        
+        JSpinner spinner_2 = new JSpinner(new SpinnerNumberModel(0.0, 0.0, 999.0,
+                1));
+        spinner_2.setMinimumSize(new Dimension(w*2,h));
+        panel.add(spinner_2);
         panel.add(inv_plante4);
-
-	  	
-	  	JButton btnNewButton = new JButton("Inventaire");
-	  	btnNewButton.setActionCommand("Inventaire");
-	  	
-	  	BufferedImage iconCompoVide=null;
-	  	try {
-	  		iconCompoVide = ImageIO.read(new File("images/vide.png"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	  	
-	  	JPanel panel_2 = new JPanel();
-	  	add(panel_2, "cell 1 7,grow");
-	  	add(btnNewButton, "cell 0 9");
-	  	
-	  	JLabel compo1,compo2,compo3, compo4;
-	  	compo1 = new JLabel();
-	  	compo2 = new JLabel();
-	  	compo3 = new JLabel();
-	  	compo4 = new JLabel();
-	  	compo1.setIcon(new ImageIcon(iconCompoVide));
-	  	compo2.setIcon(new ImageIcon(iconCompoVide));
-	  	compo3.setIcon(new ImageIcon(iconCompoVide));
-	  	compo4.setIcon(new ImageIcon(iconCompoVide));
-	  	
-	  	
-	  	MouseListener m2 = new MouseListener() {
-
-            @Override
-            public void mouseClicked(MouseEvent e) {}
-
-            @Override
-            public void mousePressed(MouseEvent e) {
-            
-                JComponent jc = (JComponent)e.getSource();
-                TransferHandler th = jc.getTransferHandler();
-                th.exportAsDrag(jc, e, TransferHandler.COPY);
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {}
-
-            @Override
-            public void mouseEntered(MouseEvent e) {}
-
-            @Override
-            public void mouseExited(MouseEvent e) {}
-        };
         
-        compo1.addMouseListener(m2);
-        compo2.addMouseListener(m2);
-        compo3.addMouseListener(m2);
-        compo4.addMouseListener(m2);
+        JSpinner spinner_3 = new JSpinner(new SpinnerNumberModel(0.0, 0.0, 999.0,
+                1));
+        spinner_3.setMinimumSize(new Dimension(w*2,h));
+        panel.add(spinner_3);
         
-        compo1.setTransferHandler(new TransferHandler("icon"));
-        compo2.setTransferHandler(new TransferHandler("icon"));
-        compo3.setTransferHandler(new TransferHandler("icon"));
-        compo4.setTransferHandler(new TransferHandler("icon"));
+        JPanel panel_3 = new JPanel();
+        add(panel_3, "cell 1 7,grow");
         
-        panel_2.add(compo1);
-        panel_2.add(compo2);
-        panel_2.add(compo3);
-        panel_2.add(compo4);
-
+        JButton btnCraft= new JButton(new ImageIcon(iconCraft));
+        btnCraft.setBorderPainted(false);
+        btnCraft.setFocusPainted(false);
+        btnCraft.setContentAreaFilled(false);
+	  	panel_3.add(btnCraft);
 	  	
-	  	JButton btnNewButton_1 = new JButton("Crafter !");
-	  	add(btnNewButton_1, "cell 2 9");}
+	  	
+	  		  	
+	  }
 	  
 	  
 	  
