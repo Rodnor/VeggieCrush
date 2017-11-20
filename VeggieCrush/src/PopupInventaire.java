15b92cf6ac89e060b8e8b70ebdc2ac861d713954
keket87 <keket87@homail.fr>
2017-11-20 18:17:42 +0100

    intégration fenetre inventaire dans page craft

--- VeggieCrush/src/PopupInventaire.java
+++ VeggieCrush/src/PopupInventaire.java
@@ -32,7 +32,7 @@ public class PopupInventaire extends JFrame implements ActionListener{
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -101,7 +101,7 @@ public class PopupInventaire extends JFrame implements ActionListener{
 				    } catch (Exception e2) {
 				        JOptionPane.showMessageDialog(null, e2);
 				    }
-				    /*try {
+				    /**try {
 				    	BufferedImage iconPlante = null;
 				        for (int i = 0; i < inv.size(); i++) {
 				            
@@ -115,7 +115,7 @@ public class PopupInventaire extends JFrame implements ActionListener{
 				        }
 				    } catch (Exception e2) {
 				        JOptionPane.showMessageDialog(null, e2);
-				    }*/
+				    }/etoile/
 				    
 				    
 				    
@@ -133,7 +133,7 @@ public class PopupInventaire extends JFrame implements ActionListener{
 				}
 			}
 		});
-	}
+	}*/
 
 	/**
 	 * Create the frame.
@@ -141,9 +141,99 @@ public class PopupInventaire extends JFrame implements ActionListener{
 	public PopupInventaire() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 500, 500);
+		setTitle("Inventaire");
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		
+		BufferedImage iconVide=null;
+		setLayout(new BorderLayout());
+		
+		
+		try {
+	  		iconVide = ImageIO.read(new File("images/vide.png"));
+	  		
+	  	} catch (IOException e) {
+	  		// TODO Auto-generated catch block
+	  		e.printStackTrace();
+	  	}
+		InventaireDao invdao = new InventaireDao();
+		ArrayList<Inventaire> inv = new ArrayList<Inventaire>();
+		JPanel btnPnl1 = new JPanel();
+		inv=invdao.getInventaireByIdAccount(1);
+		int cpt=0;
+		for (int i=0; i<inv.size();i++) {
+			if(i%5==0) {
+				cpt++;
+			}
+		}
+		
+		System.out.println("cpt : "+cpt);
+		btnPnl1.setLayout(new GridLayout(cpt,10, 25, 25));
+		btnPnl1.setSize(new Dimension(500,500));
+	  	
+		
+		
+	  	ArrayList<String> projectNameList = new ArrayList<String>();
+		
+	    for (int index = 0; index < 6*cpt; index++) {
+	        projectNameList.add("Qte : 0");
+	    }
+		
+	    for (int index = 0; index < inv.size(); index++) {
+	    	projectNameList.set(index, "Qte : " + inv.get(index).getQuantite());
+	        //projectNameList.get(i)=("Qte : " + inv.get(index).getQuantite());
+	    }
+	    
+	    String[] projectNames = projectNameList.toArray(new String[0]);
+
+	    // Adding buttons to the project
+	    //JButton[] buttons = new JButton[projectNameList.size()];
+	    JLabel[] buttons = new JLabel[projectNameList.size()];
+	    BufferedImage iconPlante = null;
+	    try {
+	        for (int i = 0; i < projectNames.length; i++) {
+	        	buttons[i] = new JLabel(projectNames[i]);
+	        	if(i<inv.size()) {
+	        		iconPlante = ImageIO.read(new File("images/herbe"+String.valueOf(i+1)+".jpg"));
+	        		buttons[i].setIcon(new ImageIcon(iconPlante));
+	        	} else {
+	        		buttons[i].setIcon(new ImageIcon(iconVide));
+	        	}
+	            buttons[i].setVerticalTextPosition(JLabel.BOTTOM);
+	            buttons[i].setHorizontalTextPosition(JLabel.CENTER);
+	            btnPnl1.add(buttons[i]);
+
+	        }
+	    } catch (Exception e2) {
+	        JOptionPane.showMessageDialog(null, e2);
+	    }
+	    /*try {
+	    	BufferedImage iconPlante = null;
+	        for (int i = 0; i < inv.size(); i++) {
+	            
+	            System.out.println("images/herbe"+String.valueOf(i+1)+".jpg");
+	            iconPlante = ImageIO.read(new File("images/herbe"+String.valueOf(i+1)+".jpg"));
+	            buttons[i].setIcon(new ImageIcon(iconPlante));
+	            buttons[i].setVerticalTextPosition(JLabel.BOTTOM);
+	            buttons[i].setHorizontalTextPosition(JLabel.CENTER);
+	            btnPnl1.add(buttons[i]);
+
+	        }
+	    } catch (Exception e2) {
+	        JOptionPane.showMessageDialog(null, e2);
+	    }*/
+	    
+	    
+	    
+	    JScrollPane jscroll = new JScrollPane(btnPnl1);
+
+	    getContentPane().add(jscroll, BorderLayout.CENTER);
+
+	    setVisible(true);
+	    setSize(500, 500);
+	    setResizable(false);
+		
 	}
 
 	@Override
