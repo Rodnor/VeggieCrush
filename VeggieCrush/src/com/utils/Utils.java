819fc98712d6978b91d782b72c83e222b88248c1
chovCodeur <michel@lesecuriesdeventadour.com>
2017-11-24 11:51:32 +0100

    PASS

--- VeggieCrush/src/com/utils/Utils.java
+++ VeggieCrush/src/com/utils/Utils.java
@@ -9,6 +9,8 @@ import java.util.Random;
 import java.util.ResourceBundle;
 import java.util.UUID;
 
+import javax.mail.MessagingException;
+
 public final class Utils {
 
 	// public static String salt = "3iLh3nalluX";
@@ -68,5 +70,26 @@ public final class Utils {
 	       pass += chars.charAt(i);
 	    }
 	    return pass;
+	}
+	
+	public static void sendMail(){
+		StringBuilder stringBuilder = new StringBuilder();
+		String user = "USERNAME";
+		stringBuilder.append("Bonjour ");
+		stringBuilder.append(user);
+		stringBuilder.append(", </br></br>");
+		stringBuilder.append("Une demande de génération de nouveau mot de passe vient d'être faite sur le jeu VeggieCrush. Vous le trouverez ci après : </br> </br>");
+		stringBuilder.append(Utils.generateNewPassword(13));
+		stringBuilder.append("</br></br>Copiez-coller ce nouveau mot de passe lors de votre prochaine connexion.");
+		stringBuilder.append("</br></br>Ceci est mail généré automatiquement par l'application VeggieCrush. Merci de ne pas y répondre !");
+		
+		System.out.println(stringBuilder.toString());
+		
+		try {
+			SendMail.sendEmailSSL("traineak@3il.fr", "Nouveau mot de passe sur VeggieCrush",stringBuilder.toString());
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
