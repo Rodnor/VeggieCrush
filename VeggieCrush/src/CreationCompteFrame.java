b7d047a6a4e5c432adaad43a6d375604076204b1
chovCodeur <michel@lesecuriesdeventadour.com>
2017-11-27 12:22:46 +0100

    commit

--- VeggieCrush/src/CreationCompteFrame.java
+++ VeggieCrush/src/CreationCompteFrame.java
@@ -56,70 +56,69 @@ public class CreationCompteFrame implements ActionListener {
 	public CreationCompteFrame() {
 		initialize();
 	}
-	
+
 	private void initialize() {
 		frame = new JFrame();
-	    frame.setResizable(false);
+		frame.setResizable(false);
 		frame.setBounds(100, 100, 450, 400);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		frame.setContentPane(contentPane);
 		frame.setTitle("Créer un compte");
-		
+
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.setVisible(true);
 		frame.setLocationRelativeTo(null);
-		
-		
+
 		contentPane.setLayout(new MigLayout("", "[grow][grow,center][grow]", "[][][][grow][][][][][][][grow,bottom]"));
-		
+
 		JLabel lblPseudo = new JLabel("Pseudo :");
 		contentPane.add(lblPseudo, "cell 1 0");
-		
+
 		pseudo = new JTextField();
 		contentPane.add(pseudo, "cell 1 1,growx");
 		pseudo.setColumns(10);
-		
+
 		lblFaction = new JLabel("Faction : ");
 		contentPane.add(lblFaction, "cell 1 2");
-		
+
 		panel = new JPanel();
 		contentPane.add(panel, "cell 1 3,grow");
 		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		
+
 		rdbtnFaction = new JRadioButton("Faction 1");
 		rdbtnFaction.addActionListener(this);
 		rdbtnFaction.setActionCommand("Faction 1");
 		panel.add(rdbtnFaction);
-		
+
 		rdbtnFaction_1 = new JRadioButton("Faction 2");
 		rdbtnFaction_1.addActionListener(this);
 		rdbtnFaction_1.setActionCommand("Faction 2");
 		panel.add(rdbtnFaction_1);
-		
+
 		ButtonGroup group = new ButtonGroup();
-	    group.add(rdbtnFaction);
-	    group.add(rdbtnFaction_1);
-		
+		group.add(rdbtnFaction);
+		group.add(rdbtnFaction_1);
+
 		JLabel lblAdresseMail = new JLabel("Adresse mail : ");
 		contentPane.add(lblAdresseMail, "cell 1 4");
-		
+
 		mail = new JTextField();
 		contentPane.add(mail, "cell 1 5,growx");
 		mail.setColumns(10);
-		
+
 		JLabel lblMotDePasse = new JLabel("Mot de passe :");
 		contentPane.add(lblMotDePasse, "cell 1 6");
-		
+
 		mdp = new JPasswordField();
 		contentPane.add(mdp, "cell 1 7,growx");
-		
+
 		JLabel lblRetapezMotDe = new JLabel("Retapez mot de passe :");
 		contentPane.add(lblRetapezMotDe, "cell 1 8");
-		
+
 		mdp_confirm = new JPasswordField();
 		contentPane.add(mdp_confirm, "cell 1 9,growx");
-		
+
 		JButton btnCrerUnCompte = new JButton("Créer un compte");
 		btnCrerUnCompte.addActionListener(this);
 		btnCrerUnCompte.setActionCommand("creer");
@@ -128,74 +127,89 @@ public class CreationCompteFrame implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+
 		StringBuilder stringBuilder = new StringBuilder();
-		if(e.getSource() instanceof JButton) {
+		if (e.getSource() instanceof JButton) {
 			JButton btn = (JButton) e.getSource();
 
-			if(btn.getActionCommand().equals("creer")) {
-				if(!pseudo.getText().equals("") && !mail.getText().equals("") 
-						&& Utils.validate(mail.getText())
-						&& !String.valueOf(mdp.getPassword()).equals("") 
+			if (btn.getActionCommand().equals("creer")) {
+				if (!pseudo.getText().equals("") && !mail.getText().equals("") && Utils.validate(mail.getText())
+						&& !String.valueOf(mdp.getPassword()).equals("")
 						&& !String.valueOf(mdp_confirm.getPassword()).equals("")
 						&& String.valueOf(mdp_confirm.getPassword()).equals(String.valueOf(mdp.getPassword()))
 						&& id_faction != 0) {
 					Account account = new Account();
-					
+					Account accountmail = new Account();
+
+					accountmail = adao.getAccountByMail(mail.getText());
 					account = adao.getAccountByUsername(pseudo.getText());
-					if (account == null){ // n'existe pas chez nous
+
+					if (account == null && accountmail == null) {// n'existe pas chez nous
 						String securePass = Utils.get_SHA_512_SecurePassword(String.valueOf(mdp.getPassword()));
-						if(Utils.usernameExistDansUneAutreAppli(pseudo.getText(), securePass) == null) { // n'existe pas ailleurs 
+						if (Utils.usernameExistDansUneAutreAppli(pseudo.getText(), securePass) == null
+								&& Utils.mailExistDansUneAutreAppli(mail.getText(), securePass) == null) { // n'existe pas ailleurs
 							String uuidString = Utils.generateUuid().toString();
-							account = new Account(0, uuidString, pseudo.getText(), mail.getText(), securePass, id_faction, null, null, null);
-							adao.insertNewAccount(account);		
+							account = new Account(0, uuidString, pseudo.getText(), mail.getText(), securePass,id_faction, null, null, null);
+							adao.insertNewAccount(account);
 							this.frame.dispose();
-						} else { 
-							// erreur, existe chez les autres + retourne le nom de l'Appli
-							// Utils.usernameExistDansUneAutreAppli(pseudo.getText(), securePass) renvoi le nom de l'appli
-							JOptionPane.showMessageDialog(null, "Ce nom d'utilisateur est déjà utilisé. Veuillez en utiliser un autre", "Utilisateur invalide", JOptionPane.ERROR_MESSAGE, null);
+						} else {
+							// Compte existe déja ailleurs
+							JOptionPane.showMessageDialog(null,
+									"Ce nom d'utilisateur est déjà utilisé. Veuillez en utiliser un autre",
+									"Utilisateur invalide", JOptionPane.ERROR_MESSAGE, null);
 						}
 					} else {
 						// Compte deja existant chez nous
-						JOptionPane.showMessageDialog(null, "Ce nom d'utilisateur est déjà utilisé. Veuillez en utiliser un autre", "Utilisateur invalide", JOptionPane.ERROR_MESSAGE, null);
+						JOptionPane.showMessageDialog(null,
+								"Ce nom d'utilisateur est déjà utilisé. Veuillez en utiliser un autre",
+								"Utilisateur invalide", JOptionPane.ERROR_MESSAGE, null);
 					}
 				} else {
-					if (pseudo.getText().equals("")){
-						
+					if (pseudo.getText().equals("")) {
+
 						stringBuilder.append("- Vous devez choisir un nom d'utilisateur");
 					}
-					if (mail.getText().equals("")){
-						if (stringBuilder.length() != 0 ){
+					
+					if (id_faction == 0) {
+						if (stringBuilder.length() != 0) {
+							stringBuilder.append("\n");
+						}
+						stringBuilder.append("- Vous devez selectionner une faction !");
+					}
+					
+					if (mail.getText().equals("")) {
+						if (stringBuilder.length() != 0) {
 							stringBuilder.append("\n");
 						}
 						stringBuilder.append("- Vous devez saisir une adresse mail");
 					}
 					if (!Utils.validate(mail.getText())) {
-						if (stringBuilder.length() != 0 ){
+						if (stringBuilder.length() != 0) {
 							stringBuilder.append("\n");
 						}
 						stringBuilder.append("- Cette adresse mail n'a pas un format valide");
 					}
-					if (String.valueOf(mdp.getPassword()).equals("") 
+					if (String.valueOf(mdp.getPassword()).equals("")
 							|| String.valueOf(mdp_confirm.getPassword()).equals("")
 							|| !(String.valueOf(mdp_confirm.getPassword()).equals(String.valueOf(mdp.getPassword())))) {
-						if (stringBuilder.length() != 0 ){
+						if (stringBuilder.length() != 0) {
 							stringBuilder.append("\n");
 						}
-						stringBuilder.append("- Les 2 mots de passe ne sont pas identiques");	
+						stringBuilder.append("- Les 2 mots de passe ne sont pas identiques");
 					}
-					
+
 					System.out.println(stringBuilder.toString());
-					JOptionPane.showMessageDialog(null, stringBuilder.toString(), "Erreur de correspondance", JOptionPane.ERROR_MESSAGE, null);
+					JOptionPane.showMessageDialog(null, stringBuilder.toString(), "Erreur de correspondance",
+							JOptionPane.ERROR_MESSAGE, null);
 
 				}
 			}
 		}
-		
-		if(e.getSource() instanceof JRadioButton) {
+
+		if (e.getSource() instanceof JRadioButton) {
 			JRadioButton btn = (JRadioButton) e.getSource();
-			
-			if(btn.getActionCommand().equals("Faction 1")) {
+
+			if (btn.getActionCommand().equals("Faction 1")) {
 				id_faction = 1;
 			} else {
 				id_faction = 2;
