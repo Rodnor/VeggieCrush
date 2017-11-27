3554cd3d3ad55fd0c3bc2d9ec72ba79e16d1e913
Rodnor <tristan.lascombe@gmail.com>
2017-11-27 12:18:53 +0100

    des trucs

--- VeggieCrush/src/CreationCompteFrame.java
+++ VeggieCrush/src/CreationCompteFrame.java
@@ -16,7 +16,11 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
+import javax.swing.JList;
+import java.awt.FlowLayout;
+import javax.swing.JRadioButton;
 
 public class CreationCompteFrame implements ActionListener {
 
@@ -27,6 +31,11 @@ public class CreationCompteFrame implements ActionListener {
 	private JPasswordField mdp;
 	private JPasswordField mdp_confirm;
 	private AccountDao adao = new AccountDao();
+	private JLabel lblFaction;
+	private JPanel panel;
+	private JRadioButton rdbtnFaction;
+	private JRadioButton rdbtnFaction_1;
+	private int id_faction = 0;
 
 	/**
 	 * Launch the application.
@@ -51,7 +60,7 @@ public class CreationCompteFrame implements ActionListener {
 	private void initialize() {
 		frame = new JFrame();
 	    frame.setResizable(false);
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 450, 400);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		frame.setContentPane(contentPane);
@@ -62,7 +71,7 @@ public class CreationCompteFrame implements ActionListener {
 		frame.setLocationRelativeTo(null);
 		
 		
-		contentPane.setLayout(new MigLayout("", "[grow][grow,center][grow]", "[][][][][][][][][grow,bottom]"));
+		contentPane.setLayout(new MigLayout("", "[grow][grow,center][grow]", "[][][][grow][][][][][][][grow,bottom]"));
 		
 		JLabel lblPseudo = new JLabel("Pseudo :");
 		contentPane.add(lblPseudo, "cell 1 0");
@@ -71,29 +80,50 @@ public class CreationCompteFrame implements ActionListener {
 		contentPane.add(pseudo, "cell 1 1,growx");
 		pseudo.setColumns(10);
 		
+		lblFaction = new JLabel("Faction : ");
+		contentPane.add(lblFaction, "cell 1 2");
+		
+		panel = new JPanel();
+		contentPane.add(panel, "cell 1 3,grow");
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		rdbtnFaction = new JRadioButton("Faction 1");
+		rdbtnFaction.addActionListener(this);
+		rdbtnFaction.setActionCommand("Faction 1");
+		panel.add(rdbtnFaction);
+		
+		rdbtnFaction_1 = new JRadioButton("Faction 2");
+		rdbtnFaction_1.addActionListener(this);
+		rdbtnFaction_1.setActionCommand("Faction 2");
+		panel.add(rdbtnFaction_1);
+		
+		ButtonGroup group = new ButtonGroup();
+	    group.add(rdbtnFaction);
+	    group.add(rdbtnFaction_1);
+		
 		JLabel lblAdresseMail = new JLabel("Adresse mail : ");
-		contentPane.add(lblAdresseMail, "cell 1 2");
+		contentPane.add(lblAdresseMail, "cell 1 4");
 		
 		mail = new JTextField();
-		contentPane.add(mail, "cell 1 3,growx");
+		contentPane.add(mail, "cell 1 5,growx");
 		mail.setColumns(10);
 		
 		JLabel lblMotDePasse = new JLabel("Mot de passe :");
-		contentPane.add(lblMotDePasse, "cell 1 4");
+		contentPane.add(lblMotDePasse, "cell 1 6");
 		
 		mdp = new JPasswordField();
-		contentPane.add(mdp, "cell 1 5,growx");
+		contentPane.add(mdp, "cell 1 7,growx");
 		
 		JLabel lblRetapezMotDe = new JLabel("Retapez mot de passe :");
-		contentPane.add(lblRetapezMotDe, "cell 1 6");
+		contentPane.add(lblRetapezMotDe, "cell 1 8");
 		
 		mdp_confirm = new JPasswordField();
-		contentPane.add(mdp_confirm, "cell 1 7,growx");
+		contentPane.add(mdp_confirm, "cell 1 9,growx");
 		
 		JButton btnCrerUnCompte = new JButton("Créer un compte");
 		btnCrerUnCompte.addActionListener(this);
 		btnCrerUnCompte.setActionCommand("creer");
-		contentPane.add(btnCrerUnCompte, "cell 1 8");
+		contentPane.add(btnCrerUnCompte, "cell 1 10");
 	}
 
 	@Override
@@ -108,7 +138,8 @@ public class CreationCompteFrame implements ActionListener {
 						&& Utils.validate(mail.getText())
 						&& !String.valueOf(mdp.getPassword()).equals("") 
 						&& !String.valueOf(mdp_confirm.getPassword()).equals("")
-						&& String.valueOf(mdp_confirm.getPassword()).equals(String.valueOf(mdp.getPassword()))) {
+						&& String.valueOf(mdp_confirm.getPassword()).equals(String.valueOf(mdp.getPassword()))
+						&& id_faction != 0) {
 					Account account = new Account();
 					
 					account = adao.getAccountByUsername(pseudo.getText());
@@ -116,7 +147,7 @@ public class CreationCompteFrame implements ActionListener {
 						String securePass = Utils.get_SHA_512_SecurePassword(String.valueOf(mdp.getPassword()));
 						if(Utils.usernameExistDansUneAutreAppli(pseudo.getText(), securePass) == null) { // n'existe pas ailleurs 
 							String uuidString = Utils.generateUuid().toString();
-							account = new Account(0, uuidString, pseudo.getText(), mail.getText(), securePass, 1, null, null, null);
+							account = new Account(0, uuidString, pseudo.getText(), mail.getText(), securePass, id_faction, null, null, null);
 							adao.insertNewAccount(account);		
 							this.frame.dispose();
 						} else { 
@@ -158,6 +189,16 @@ public class CreationCompteFrame implements ActionListener {
 					JOptionPane.showMessageDialog(null, stringBuilder.toString(), "Erreur de correspondance", JOptionPane.ERROR_MESSAGE, null);
 
 				}
+			}
+		}
+		
+		if(e.getSource() instanceof JRadioButton) {
+			JRadioButton btn = (JRadioButton) e.getSource();
+			
+			if(btn.getActionCommand().equals("Faction 1")) {
+				id_faction = 1;
+			} else {
+				id_faction = 2;
 			}
 		}
 	}
