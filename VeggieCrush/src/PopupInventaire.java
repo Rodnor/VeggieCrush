c2c9befaab3bb08bf341444e4e2ddb3094d59601
keket87 <keket87@homail.fr>
2017-12-04 19:40:00 +0100

    refresh inventaire

--- VeggieCrush/src/PopupInventaire.java
+++ VeggieCrush/src/PopupInventaire.java
@@ -5,6 +5,8 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -28,6 +30,16 @@ import com.entitie.Inventaire;
 public class PopupInventaire extends JFrame implements ActionListener{
 
 	private JPanel contentPane;
+	private Thread t;
+	private boolean run=true;
+	ArrayList<Inventaire> inv;
+	ArrayList<String> projectNameList;
+	JLabel[] buttons;
+	String[] projectNames;
+	BufferedImage iconPlante = null;
+	BufferedImage iconVide=null;
+	JScrollPane jscroll;
+	JPanel btnPnl1;
 
 	/**
 	 * Create the frame.
@@ -40,82 +52,108 @@ public class PopupInventaire extends JFrame implements ActionListener{
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
-		BufferedImage iconVide=null;
-		setLayout(new BorderLayout());
+		addWindowListener(new WindowAdapter() {
+		    @Override
+		    public void windowClosing(WindowEvent e) {
+		       	run=false;
+		    }
+		});
 		
+		setLayout(new BorderLayout());
+
+	    
+	    setSize(500, 500);
+	    setResizable(false);
 		
 		try {
 	  		iconVide = ImageIO.read(new File("images/vide.png"));
 	  		
 	  	} catch (IOException e) {
-	  		// TODO Auto-generated catch block
 	  		e.printStackTrace();
 	  	}
+		
 		InventaireDao invdao = new InventaireDao();
-		ArrayList<Inventaire> inv = new ArrayList<Inventaire>();
-		JPanel btnPnl1 = new JPanel();
+
 		
-		inv=invdao.getInventaireByUuid(MainFrame.getUUID());
-		int cpt=0;
-		for (int i=0; i<inv.size();i++) {
-			if(i%6==0) {
-				cpt++;
+		
+		t = new Thread() {
+			public void run() {
+				while(run) {
+					btnPnl1 = new JPanel();
+					getContentPane().removeAll();
+					
+					inv = new ArrayList<Inventaire>();
+					inv=invdao.getInventaireByUuid(MainFrame.getUUID());
+					int cpt=0;
+					for (int i=0; i<inv.size();i++) {
+						if(i%6==0) {
+							cpt++;
+						}
+					}
+					
+					btnPnl1.setLayout(new GridLayout(cpt,10, 25, 25));
+					btnPnl1.setSize(new Dimension(500,500));
+				  	
+
+				  	projectNameList = new ArrayList<String>();
+					
+				    for (int index = 0; index < 6*cpt; index++) {
+				        projectNameList.add("");
+				    }
+					
+				    for (int index = 0; index < inv.size(); index++) {
+				    	projectNameList.set(index, "Qte : " + inv.get(index).getQte());
+				    }
+				    
+				    projectNames = projectNameList.toArray(new String[0]);
+			
+				    buttons = new JLabel[projectNameList.size()];
+				    
+				    try {
+				        for (int i = 0; i < projectNames.length; i++) {
+				        	buttons[i] = new JLabel(projectNames[i]);
+				        	if(i<inv.size()) {
+				        		if(i<4) {
+				        			iconPlante = ImageIO.read(new File("images/herbe"+String.valueOf(i+1)+".jpg"));
+					        		buttons[i].setIcon(new ImageIcon(iconPlante));
+				        		} else {
+				        			iconPlante = ImageIO.read(new File("images/recette"+String.valueOf(inv.get(i).getId_objet()-4)+".png"));
+					        		buttons[i].setIcon(new ImageIcon(iconPlante));
+				        		}
+				        		
+				        	} else {
+				        		buttons[i].setIcon(new ImageIcon(iconVide));
+				        	}
+				            buttons[i].setVerticalTextPosition(JLabel.BOTTOM);
+				            buttons[i].setHorizontalTextPosition(JLabel.CENTER);
+				            btnPnl1.add(buttons[i]);
+			
+				        }
+				    } catch (Exception e2) {
+				        JOptionPane.showMessageDialog(null, e2);
+				    }
+				    
+				    jscroll = new JScrollPane(btnPnl1);
+				    
+				    getContentPane().add(jscroll, BorderLayout.CENTER);
+				    
+				    setVisible(true);
+							    					
+			        
+					try {
+						Thread.sleep(500);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				if(!run) {
+
+			        t.interrupt();
+				}
 			}
-		}
-		
-		btnPnl1.setLayout(new GridLayout(cpt,10, 25, 25));
-		btnPnl1.setSize(new Dimension(500,500));
-	  	
-		
-		
-	  	ArrayList<String> projectNameList = new ArrayList<String>();
-		
-	    for (int index = 0; index < 6*cpt; index++) {
-	        projectNameList.add("");
-	    }
-		
-	    for (int index = 0; index < inv.size(); index++) {
-	    	projectNameList.set(index, "Qte : " + inv.get(index).getQte());
-	    }
-	    
-	    String[] projectNames = projectNameList.toArray(new String[0]);
-
-	    JLabel[] buttons = new JLabel[projectNameList.size()];
-	    BufferedImage iconPlante = null;
-	    try {
-	        for (int i = 0; i < projectNames.length; i++) {
-	        	buttons[i] = new JLabel(projectNames[i]);
-	        	if(i<inv.size()) {
-	        		if(i<4) {
-	        			iconPlante = ImageIO.read(new File("images/herbe"+String.valueOf(i+1)+".jpg"));
-		        		buttons[i].setIcon(new ImageIcon(iconPlante));
-	        		} else {
-	        			iconPlante = ImageIO.read(new File("images/recette"+String.valueOf(inv.get(i).getId_objet()-4)+".png"));
-		        		buttons[i].setIcon(new ImageIcon(iconPlante));
-	        		}
-	        		
-	        	} else {
-	        		buttons[i].setIcon(new ImageIcon(iconVide));
-	        	}
-	            buttons[i].setVerticalTextPosition(JLabel.BOTTOM);
-	            buttons[i].setHorizontalTextPosition(JLabel.CENTER);
-	            btnPnl1.add(buttons[i]);
-
-	        }
-	    } catch (Exception e2) {
-	        JOptionPane.showMessageDialog(null, e2);
-	    }
-	    
-	    
-	    
-	    JScrollPane jscroll = new JScrollPane(btnPnl1);
-
-	    getContentPane().add(jscroll, BorderLayout.CENTER);
-
-	    setVisible(true);
-	    setSize(500, 500);
-	    setResizable(false);
-		
+		};
+		t.start();
+	   
 	}
 
 	@Override
